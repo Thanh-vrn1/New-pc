f6fcf36c7ad5fada2ab22745264bcc2ee738eec8
vigneron <163116378+Thanh-vrn1@users.noreply.github.com>
2024-04-15 11:22:49 +0200

    Update Thanh installation app.bat

--- 2 - installation logiciel/1 - Installation All in one/Winget/Thanh installation app.bat
+++ 2 - installation logiciel/1 - Installation All in one/Winget/Thanh installation app.bat
@@ -1,30 +1,27 @@
 start
-start https://bit.ly/3BlS71b
-winget install Microsoft.WindowsTerminal 
-winget install Microsoft.PowerToys 
-winget install EpicGames.EpicGamesLauncher 
-winget install KDE.Krita 
-winget install Ablaze.Floorp 
-winget install Peppy.Osu! 
-winget install OBSProject.OBSStudio 
-winget install 9PF4KZ2VN4W9 
-winget install 9NBLGGH516XP 
-winget install Opera.OperaGX 
-winget install Discord.Discord 
-winget install 9MSMLRH6LZF3 
-winget install 9NCVDN91XZQP 
-winget install Malwarebytes.Malwarebytes 
-winget install XP9KHM4BK9FZ7Q
-winget install RARLab.WinRAR
-winget install 9NTM2QC6QWS7
-winget install Rainmeter.Rainmeter
-winget install Twitch.TwitchStudio
-winget install Streamlabs.Streamlabs
-start https://dsadata.intel.com/installer
-start https://brightvpn.com/?lang=fr#elementor-action%3Aaction%3Dpopup%3Aopen%26settings%3DeyJpZCI6MTA4OSwidG9nZ2xlIjpmYWxzZX0%3D
-start https://filezilla-project.org/download.php?type=client
-start ms-windows-store://pdp?hl=en-us&gl=us&productid=9nq07fg50h2q&mode=mini&pos=-9%2C-9%2C1937%2C1157&referrer=storeforweb&source=https%3A%2F%2Fwww.google.com%2F&ocid=psi_c_bf
-start https://bakkesmod.com/download.php
-start https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe
+start https://bit.ly/3BlS71b 
+winget install Microsoft.PowerToys                REM POWERTOYS
+winget install EpicGames.EpicGamesLauncher        REM EPIC GAMES LAUNCHER
+winget install KDE.Krita                          REM KRITA
+winget install Ablaze.Floorp                      REM FLOORP
+winget install Peppy.Osu!                         REM OSU
+winget install OBSProject.OBSStudio               REM OBS STUDIO
+winget install 9PF4KZ2VN4W9                       REM TRANSLUCENT TB
+winget install 9NBLGGH516XP                       REM EAR TRUMPET
+winget install Opera.OperaGX                      REM OPERA GX
+winget install Discord.Discord                    REM DISCORD
+winget install 9NCVDN91XZQP                       REM PYTHON
+winget install Malwarebytes.Malwarebytes          REM MALWAREBYTES
+winget install XP9KHM4BK9FZ7Q                     REM VISUAL STUDIO CODE
+winget install RARLab.WinRAR                      REM WINRAR
+winget install 9NTM2QC6QWS7                       REM LIVELY WALLPAPER
+winget install Rainmeter.Rainmeter                REM RAINMETER
+winget install Twitch.TwitchStudio                REM TWITCH STUDIO
+winget install Streamlabs.Streamlabs              REM STREAMLABS
+start https://dsadata.intel.com/installer         REM INTEL DRIVERS SUPPORT
+start https://brightvpn.com/?lang=fr#elementor-action%3Aaction%3Dpopup%3Aopen%26settings%3DeyJpZCI6MTA4OSwidG9nZ2xlIjpmYWxzZX0%3D REM BRIGHTVPN
+start https://filezilla-project.org/download.php?type=client               REM FILEZILLA
+start https://bakkesmod.com/download.php                                   REM BAKKESMOD
+start https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe   REM STEAM
 
 exit
